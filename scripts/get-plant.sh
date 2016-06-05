3caec49b3854c81a2426732aa92d3dc57ef8af0a
MichaelB167 <michael.m.bates@gmail.com>
2016-06-05 16:55:30 -0400

    Working curl script to get a single plant

---
+++ scripts/get-plant.sh
@@ -0,0 +1,4 @@
+TOKEN='BAhJIiUyZjEyOWRiODQ1YzBhMWYxMmViMGE5ODRjMGI2NmNkZQY6BkVG--2d6c5458e3c7fc403aa320f41559a0446e4f6873'
+curl --include --request GET http://localhost:3000/plants/1 \
+  --header "Authorization: Token token=$TOKEN" \
+  --header "Content-Type: application/json" \
