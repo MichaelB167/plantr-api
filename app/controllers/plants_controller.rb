b53c859ffabd768cce66c410b131252c90f28ef6
MichaelB167 <michael.m.bates@gmail.com>
2016-06-05 15:39:53 -0400

    Add controller for plants

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,62 @@
+# Plant controller
+class PlantsController < ProtectedController
+  before_action :set_plant, only: [:update, :destroy]
+
+  # GET /plants
+  # GET /plants.json
+  def index
+    @plants = current_user.plants
+
+    render json: @plants
+  end
+
+  # GET /plants/1
+  # GET /plants/1.json
+  def show
+    render json: @plant
+  end
+
+  # POST /plants
+  # POST /plants.json
+  def create
+    @plant = current_user.plants.build(plant_params)
+
+    if @plant.save
+      render json: @plant, status: :created, location: @plant
+    else
+      render json: @plant.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /plants/1
+  # PATCH/PUT /plants/1.json
+  def update
+    @plant = current_user.plants.find(params[:id])
+
+    if @plant.update(plant_params)
+      head :no_content
+    else
+      render json: @plant.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /plants/1
+  # DELETE /plants/1.json
+  def destroy
+    @plant.destroy
+
+    head :no_content
+  end
+
+  def set_plant
+    @plant = current_user.plants.find(params[:id])
+  end
+
+  def plant_params
+    params.require(:plant).permit(:category, :harvest, :name, :quantity,
+                                  :planted_on, :expected_harvest, :care_notes,
+                                  :user, :user_id)
+  end
+
+  private :set_plant, :plant_params
+end
