c0130dff4e3cd7a797e92e8a264fde68d7973a87
MichaelB167 <michael.m.bates@gmail.com>
2016-06-05 15:23:46 -0400

    Add schema post migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160327214720) do
+ActiveRecord::Schema.define(version: 20160605191551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,21 @@ ActiveRecord::Schema.define(version: 20160327214720) do
 
   add_index "examples", ["user_id"], name: "index_examples_on_user_id", using: :btree
 
+  create_table "plants", force: :cascade do |t|
+    t.string   "category"
+    t.string   "harvest"
+    t.string   "name"
+    t.integer  "quantity"
+    t.date     "planted_on"
+    t.date     "expected_harvest"
+    t.string   "care_notes"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.integer  "user_id"
+  end
+
+  add_index "plants", ["user_id"], name: "index_plants_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
     t.string   "token",           null: false
@@ -37,4 +52,5 @@ ActiveRecord::Schema.define(version: 20160327214720) do
   add_index "users", ["token"], name: "index_users_on_token", unique: true, using: :btree
 
   add_foreign_key "examples", "users"
+  add_foreign_key "plants", "users"
 end
