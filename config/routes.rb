37fc2e9c1e3b6687d265c33a851b95f17e26f73b
MichaelB167 <michael.m.bates@gmail.com>
2016-06-05 16:48:49 -0400

    Change resource to resources in plants routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  resources :plants, except: [:new, :edit]
   resources :examples, except: [:new, :edit]
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
   resources :users, only: [:index, :show]
-  resource :plants, except: [:index, :show]
 end
