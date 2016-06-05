7dfa3001b88c9f7d9bcfbd9f59a67b32ba9ca4aa
MichaelB167 <michael.m.bates@gmail.com>
2016-06-05 14:11:30 -0400

    change instances of rails api template to plantr api

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ require 'action_controller/railtie'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RailsApiTemplate
+module PlantrApi
   # :nodoc:
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified
