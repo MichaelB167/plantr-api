88a32cce2a7557c69762961a44bf013096eb645f
MichaelB167 <michael.m.bates@gmail.com>
2016-06-08 14:52:13 -0400

    Working curl script for food2fork api

---
+++ scripts/food2fork.sh
@@ -0,0 +1,3 @@
+curl --get --include 'https://community-food2fork.p.mashape.com/get?key=5b778f1043cebc3d2ef97301d9e0b8ca&rId=37859' \
+  -H 'X-Mashape-Key: L6g81XjLIbmshggOcSE8HpgxKm8lp1rvJFLjsnYRjyfhPr7pQc' \
+  -H 'Accept: application/json'
