6ab0d8d64d73683a8e3eb18ee7144a41aa886273
MichaelB167 <michael.m.bates@gmail.com>
2016-06-08 13:30:35 -0400

    Change zipcode type to 'string' in plants schema Allows for zipcodes starting with '0' to fully save

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160608140956) do
+ActiveRecord::Schema.define(version: 20160608171326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 20160608140956) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.integer  "user_id"
-    t.integer  "zipcode"
+    t.string   "zipcode"
   end
 
   add_index "plants", ["user_id"], name: "index_plants_on_user_id", using: :btree
