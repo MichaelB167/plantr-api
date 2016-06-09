cdc6839cd0b4081f010debe7a9b515f1d38f61bc
MichaelB167 <michael.m.bates@gmail.com>
2016-06-09 13:20:24 -0400

    Create route for Food2Fork api recipe request

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
   resources :plants, except: [:new, :edit]
   resources :examples, except: [:new, :edit]
   post '/sign-up' => 'users#signup'
