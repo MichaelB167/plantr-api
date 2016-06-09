1fe56bc2c9e8f6714594e9c0e1d97de613c6b8c7
MichaelB167 <michael.m.bates@gmail.com>
2016-06-09 13:21:10 -0400

    Working curl script for food2fork Can now second request through rails for recipes through the second one

--- scripts/food2fork.sh
+++ scripts/food2fork.sh
@@ -1,3 +1,7 @@
 curl --get --include 'https://community-food2fork.p.mashape.com/get?key=5b778f1043cebc3d2ef97301d9e0b8ca&rId=37859' \
   -H 'X-Mashape-Key: L6g81XjLIbmshggOcSE8HpgxKm8lp1rvJFLjsnYRjyfhPr7pQc' \
   -H 'Accept: application/json'
+
+
+curl --include --request GET http://localhost:3000/recipes/index \
+    --header "Content-Type: application/json" \
